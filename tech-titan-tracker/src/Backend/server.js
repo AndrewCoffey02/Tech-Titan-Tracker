27af15d0ab4ef17c57d523153848d9580f858325
Andrew Coffey <Andrewcoffey@hotmail.com>
2024-03-21 14:47:55 +0000

    Added Graph to canvas and commented backend side

--- tech-titan-tracker/src/Backend/server.js
+++ tech-titan-tracker/src/Backend/server.js
@@ -4,10 +4,12 @@ const redis = require('redis')
 const app = express()
 const port = 4000
 
+//call the client access
 const Client = redis.createClient({
     url: 'redis://localhost:6379'
 });
 
+//connect to redis database
 Client.on('connect', () => {
     console.log('Connected to redis server succcesfully');
 });
@@ -15,11 +17,12 @@ Client.on('connect', () => {
 Client.on('error', (error) => {
     console.error('Redis client encountered an error:', error);
 });
-  
+//wait for client to connect
 (async () => {
     await Client.connect();
 })();
 
+//request what port for database to listen to 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
