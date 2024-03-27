bea4a4866ce2c20e1d6104e0f3b97907ad088c44
Andrew Coffey <Andrewcoffey@hotmail.com>
2024-03-27 22:30:52 +0000

    fixed errors and updated to only read from upstash

--- tech-titan-tracker/src/Backend/Routes.js
+++ tech-titan-tracker/src/Backend/Routes.js
@@ -2,13 +2,7 @@
 import axios from 'axios'
 import { Redis } from 'ioredis'
 
-const client = new Redis("rediss://default:9105239db83042b1b00a4d3d6f8011e5@eu1-becoming-kite-39003.upstash.io:39003");
-
-
-const redis = new Redis({
-    'port': 6379,
-    'host': '127.0.0.1'
-})
+const redis = new Redis("rediss://default:9105239db83042b1b00a4d3d6f8011e5@eu1-becoming-kite-39003.upstash.io:39003");
 
 //call the apikey with authorisation
 const apiKey = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=af494e602866b5ff00ccf447bbdaf847`
@@ -30,7 +24,7 @@ const getLocation = async (city) => {
 
     // call response with axios
     let response = await axios.get(apiKey(city))
-    await client.set(`weather: ${city}`, JSON.stringify(response.data))
+    redis.set(`weather : ${city}`, JSON.stringify(response.data))
     //return response
     return {...response.data, 'source' : 'API' }
 
