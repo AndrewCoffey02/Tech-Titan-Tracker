1fc284dbbfbb33721c1809416b536351a3d1a2c0
Andrew Coffey <Andrewcoffey@hotmail.com>
2024-03-27 20:20:59 +0000

    api response is cached to redis database

--- tech-titan-tracker/src/Backend/Routes.js
+++ tech-titan-tracker/src/Backend/Routes.js
@@ -1,16 +1,44 @@
-import axios from 'axios';
+//importing tools
+import axios from 'axios'
+import { Redis } from 'ioredis'
 
-const apiKey = (lat, lon) => 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=af494e602866b5ff00ccf447bbdaf847'
+const redis = new Redis({
+    'port': 6379,
+    'host': '127.0.0.1'
+})
 
-const getLocation = async (lat, lon) => {
+//call the apikey with authorisation
+const apiKey = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=af494e602866b5ff00ccf447bbdaf847`
 
-    let response = await axios.get(apiKey(lat, lon))
+//get location with longitude and latitude
+const getLocation = async (city) => {
 
+    let cacheEntry = await redis.get(`weather : ${city}`)
+
+    //if we have a cache hit
+    if(cacheEntry) {
+
+        cacheEntry = JSON.parse(cacheEntry)
+
+        //return that entry
+        return {...cacheEntry, 'source' : 'cache'}
+    }
+
+    //if we have a miss
+
+    // call response with axios
+    let response = await axios.get(apiKey(city))
+
+    //return response
     return {...response.data, 'source' : 'API' }
 
 }
 
-const lat = '44.34'
-const lon = '10.99'
-let weather = await getLocation(apiKey)
+const city = 'Galway'
+const t0 = new Date().getTime()
+let weather = await getLocation(city)
+const t1 = new Date().getTime()
+weather.responseTime = `${t1-t0}ms`
+
 console.log(weather)
+process.exit()
