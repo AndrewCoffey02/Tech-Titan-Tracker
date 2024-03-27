a2e29fb95ec6177fd4fea8541223f17bfa2d0281
Andrew Coffey <Andrewcoffey@hotmail.com>
2024-03-27 20:32:35 +0000

    test api adding cached data to upstash

--- tech-titan-tracker/src/Backend/Routes.js
+++ tech-titan-tracker/src/Backend/Routes.js
@@ -2,6 +2,9 @@
 import axios from 'axios'
 import { Redis } from 'ioredis'
 
+const client = new Redis("rediss://default:9105239db83042b1b00a4d3d6f8011e5@eu1-becoming-kite-39003.upstash.io:39003");
+
+
 const redis = new Redis({
     'port': 6379,
     'host': '127.0.0.1'
@@ -13,13 +16,12 @@ const apiKey = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${ci
 //get location with longitude and latitude
 const getLocation = async (city) => {
 
-    let cacheEntry = await redis.get(`weather : ${city}`)
-
+    let cacheEntry = await redis.get(`weather : ${city}`) 
+    
     //if we have a cache hit
     if(cacheEntry) {
 
         cacheEntry = JSON.parse(cacheEntry)
-
         //return that entry
         return {...cacheEntry, 'source' : 'cache'}
     }
@@ -28,7 +30,7 @@ const getLocation = async (city) => {
 
     // call response with axios
     let response = await axios.get(apiKey(city))
-
+    await client.set(`weather: ${city}`, JSON.stringify(response.data))
     //return response
     return {...response.data, 'source' : 'API' }
 
