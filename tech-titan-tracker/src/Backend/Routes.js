f6bd2a5c639774599e3370029f7758e968e092a0
Andrew Coffey <Andrewcoffey@hotmail.com>
2024-03-28 20:07:03 +0000

    adjusted routes.js to call api by the stock name and data type

--- tech-titan-tracker/src/Backend/Routes.js
+++ tech-titan-tracker/src/Backend/Routes.js
@@ -6,24 +6,23 @@ import { Redis } from 'ioredis'
 const redis = new Redis("rediss://default:9105239db83042b1b00a4d3d6f8011e5@eu1-becoming-kite-39003.upstash.io:39003");
 
 //call the apikey with authorisation
-const apiKey = {
-  method: 'GET',
+const IncomeStatement = (Search) => ({
+  //maybe make the endpoint options (income statement) a template literal??
   url: 'https://real-time-finance-data.p.rapidapi.com/company-income-statement',
   params: {
-    symbol: 'AAPL:NASDAQ',
-    period: 'QUARTERLY',
-    language: 'en'
+    symbol: `${Search}`,
+    period: 'QUARTERLY'
   },
   headers: {
     'X-RapidAPI-Key': '6612968778mshdc7b0e1fe333d44p147994jsne94ba23b6238',
     'X-RapidAPI-Host': 'real-time-finance-data.p.rapidapi.com'
   }
-};
+});
 
 //get location with longitude and latitude
-const NASAAP = async () => {
+const findIncomeStatement = async (Search) => {
 
-    let cacheEntry = await redis.get('NASDAQ:AAPL') 
+    let cacheEntry = await redis.get(`${Search}`) 
     
     //if we have a cache hit, it will select the key
     if(cacheEntry) {
@@ -36,19 +35,18 @@ const NASAAP = async () => {
 
     //if we have a miss
     // call response with axios
-    let response = await axios.request(apiKey)
-    redis.set('NASDAQ:AAPL', JSON.stringify(response.data))
+    let response = await axios.request(IncomeStatement(Search))
+    redis.set(`${Search}`, JSON.stringify(response.data))
     //return response
     return {...response.data, 'source' : 'API' }
 
 }
 
 //parameters
-const city = 'Galway'
-
+const Search = 'AAPL'
 //time and call the method 
 const t0 = new Date().getTime()
-let weather = await NASAAP()
+let weather = await findIncomeStatement(Search)
 const t1 = new Date().getTime()
 weather.responseTime = `${t1-t0}ms`
 console.log(weather)
