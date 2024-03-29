1fc50ecc75d75e820b3acba72a865a9c2fe81085
Andrew Coffey <Andrewcoffey@hotmail.com>
2024-03-29 12:52:33 +0000

    comments

--- tech-titan-tracker/src/Backend/Routes.js
+++ tech-titan-tracker/src/Backend/Routes.js
@@ -2,14 +2,13 @@ import express from 'express'
 import response from './server.js'
 const app = express()
 const port = '4000'
-
  
-
+//sending redis response
 app.get('/api', (req,res) => {
-    res.send(response);
+    res.json.send(response)
 })
 
-
+//app listening to port number
 app.listen(port, () => {
     console.log(`App, listening on port ${port}!`)
 })
