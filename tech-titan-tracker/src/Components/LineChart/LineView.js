aa9df14f0fb615257c6c1d4e85c5619d5a413a7b
Andrew Coffey <Andrewcoffey@hotmail.com>
2024-04-25 19:31:03 +0100

    changed dropdown to a seperate component

--- tech-titan-tracker/src/Components/LineChart/LineView.js
+++ tech-titan-tracker/src/Components/LineChart/LineView.js
@@ -1,37 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import LineChart from './LineChart'
-import data from './data.json'
+import dataa from './data.json'
 import axios from 'axios'
 
-export default function Data() {
+export default function Linechart({ option }) {
 
     const [income, setIncome] = useState([])
-    const [selected, setSelected] = useState('')
 
     useEffect(() => {
-             axios.get(`http://localhost:4000/api/${selected}`)
-                .then((res) => {
-                    setIncome(res.data.data.income_statement)
-                    console.log(income)
-                })
-                .catch((error) => { console.error(error) })
+        axios.get(`http://localhost:4000/api/${option}`)
+            .then((res) => {
+                setIncome(res.data.data.income_statement)
 
+            }).catch((error) => { console.error(error) })
     })
 
-    const handleChange = (event) => {
-        setSelected(event.target.value)
-    }
     return (
         <React.Fragment>
-                <select value={selected} onChange={handleChange}>
-                    <option >Select Company</option>
-                    <option value="AMZN">Amazon</option>
-                    <option value="MSFT">Microsoft</option>
-                    <option value="META">Meta</option>
-                    <option value="GOOGL">Google</option>
-                    <option value="AAPL">Apple</option>
-                </select>
-            <LineChart data={income}/>
+            <LineChart data={income} />
         </React.Fragment>
     )
 }
