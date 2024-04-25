05a70965d739830b1c76a09fed803b8fb56740ad
Andrew Coffey <Andrewcoffey@hotmail.com>
2024-04-25 19:30:19 +0100

    fixed barchart and modified axios to read options

--- tech-titan-tracker/src/Components/BarChart/BarChart.js
+++ tech-titan-tracker/src/Components/BarChart/BarChart.js
@@ -1,66 +1,72 @@
 import React, { useRef, useEffect, useState } from "react"
 import "./BarChart.css"
-import { select,
-        axisBottom,
-        axisRight, 
-        scaleLinear, 
-        scaleBand,
-        min,
-        max
+import {
+  select,
+  axisBottom,
+  axisRight,
+  scaleLinear,
+  scaleBand,
+  min,
+  max
 } from "d3"
 
-export default  function BarChart({data}) {
+export default function BarChart({ data }) {
   // set the dimensions and margins of the graph
-  var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-  width = 1400 - margin.left - margin.right,
-  height = 300 - margin.top - margin.bottom
-
   const svgRef = useRef()
 
-  const minYear = min(data, year => year.year)
-  const maxYear = max(data, year => year.year)
-
   useEffect(() => {
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 1400 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom
+
+    const minYear = min(data, year => year.year)
+    const maxYear = max(data, year => year.year)
 
     const svg = select(svgRef.current)
+    svg.selectAll("*").remove()
 
     const xScale = scaleBand()
-      .domain(data.map( (value, index) => index))
-      .range([100, width])
-      .padding(0.5)
+      .domain(data.map(index => index.year).reverse())
+      .range([0, width])
+      .padding(0.1)
 
     const yScale = scaleLinear()
       .domain([0, max(data, rev => rev.revenue)])
-      .range([height, 10])
+      .range([height, 0])
 
-    const xAxis = axisBottom(xScale).ticks(data.length)
-
+    const xAxis = axisBottom(xScale)
     svg
-      .select(".x-axis")
-      .style("transform", "translateY("+height+"px)")
+      .append("g")
+      .style("transform", `translateY(${height}px)`)
       .call(xAxis)
 
     const yAxis = axisRight(yScale)
     svg
-      .select(".y-axis")
-      .style("transform", "translateX(" + width + "px)")
+      .append("g")
+      .style("transform", `translateX(${width}px)`)
       .call(yAxis)
 
     svg
+      .append("g")
       .selectAll(".bar")
       .data(data)
-      .join("rect")
+      .enter().append("rect")
       .attr("class", "bar")
       
-      .style("transfrom", "scale(1, -1)")
-      .attr("x", index => xScale(index))
-      .attr("y", rev => yScale(rev.revenue.length))
-      .attr( "width", xScale.bandwidth())
-      .attr("height", d => yScale(d.value))
+      .attr("x", d => xScale(d.year))
+      .attr("width", d => xScale.bandwidth())
+      .attr("y", height)
+      .attr("height", 0)
+      .transition()
+      .duration(1000)
+      .delay((d, i) => i * 100)
+      .attr("y", d => yScale(d.revenue))
+      .attr("height", d => height - yScale(d.revenue))
       .attr("fill", "#54c47a")
+      
+      
+
   })
-
-
   return (
     <React.Fragment>
       <svg ref={svgRef}>
@@ -68,7 +74,6 @@ export default  function BarChart({data}) {
         <g className="y-axis" />
       </svg>
     </React.Fragment>
-  );
+  )
 
-  
 }
--- tech-titan-tracker/src/Components/BarChart/BarView.js
+++ tech-titan-tracker/src/Components/BarChart/BarView.js
@@ -3,12 +3,12 @@ import BarChart from './BarChart'
 import data from './data.json'
 import axios from 'axios'
 
-export default function Data() {
+export default function Data({option}) {
 
     const [income, setIncome] = useState([])
 
     useEffect(() => {
-        axios.get('http://localhost:4000/api')
+        axios.get(`http://localhost:4000/api/${option}`)
             .then((res) => {
                 setIncome(res.data.data.income_statement)
             })
